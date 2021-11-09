88a999cddd8c4ad56e8be22a6ef59ec4d711ba5e
fiha18 <fihaas786@gmail.com>
2021-11-10 00:04:29 +0530

    Binary Tree Level Order Traversal

---
+++ BinaryTreeLevelOrder.cpp
@@ -0,0 +1,26 @@
+vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int qLen = q.size();
+            vector<int> level;
+            for(int i=0; i < qLen ; i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node!= NULL)
+                {
+                    level.push_back(node->val);
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+            }
+            if(level.size()!=0)
+            {
+                res.push_back(level);
+            }
+        }
+      return res;  
+    }
